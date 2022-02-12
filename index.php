d0eced19d39ba8b16b4cfee2ecbc6a305362831a
wildarte <wildarte10@gmail.com>
2022-02-12 19:02:21 -0300

    estrutura basica

---
+++ index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="./assets/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="./assets/css/reset.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Blog EB3</title>
+</head>
+<body>
+    
+    <header>
+        <div>
+            <div>
+                <img src="" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+            </nav>
+            <div>
+                <form action="" method="post">
+                    <input type="text" name="s" id="">
+                </form>
+            </div>
+            <div>
+                <a href="#">Inscreva-se</a>
+            </div>
+        </div>
+    </header>
+
+    <main>
+
+        <section>
+            <div>
+                <div>
+                    <h1 style="font-weight: 300;">Insights para ajudar você a</h1>
+                    <i class="bi bi-alarm-fill"></i>
+                    <h2>crescer o seu negócio</h2>
+                    <p>Seja bem-vindo ao blog da Rock Content. Aprenda sobre como criar experiências de conteúdo que atraem, convertem e retêm seu público ao longo de todos os estágios da jornada de compra.</p>
+
+                    <a href="#">Inscreva-se</a>
+                </div>
+
+                <div>
+                    <img src="" alt="">
+                </div>
+            </div>
+        </section>
+
+        <section style="border: 1px solid; min-height: 200px;">
+            <div class="owl-carousel owl-theme my-carousel" style="display: flex; justify-content: flex-start;border: 1px solid">
+                <article style="border: 1px solid red; width: 200px; height: 200px">
+                    <div>
+
+                    </div>
+                    <div>
+                        <h2>Saiba o que é arquitetura centrada em dados e como deixar a sua empresa preparada</h2>
+                        <p>A arquitetura centrada em dados é uma nova mentalidade de negócio, que coloca os dados no centro das organizações. N(...)</p>
+
+                    </div>
+
+                    <div>
+                        <div>
+                            <img src="" alt="">
+                        </div>
+                        <div>
+                            02/02/2022
+                        </div>
+                    </div>
+
+                </article>
+
+            </div>
+
+            <section>
+                <article>
+                    <img src="" alt="">
+                    <h2>A fidelidade às marcas está diminuindo: como manter os clientes engajados mesmo assim</h2>
+                    <p>Um estudo com 2.000 pessoas dos EUA e do Reino Unido pela Edit, especialista em dados de clientes, e Kin + Carta, uma co(...)</p>
+                    <div>
+                        <div>
+                            <img src="" alt="">
+                        </div>
+                        <div>
+                            02/02/2022
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <img src="" alt="">
+                    <h2>A fidelidade às marcas está diminuindo: como manter os clientes engajados mesmo assim</h2>
+                    <p>Um estudo com 2.000 pessoas dos EUA e do Reino Unido pela Edit, especialista em dados de clientes, e Kin + Carta, uma co(...)</p>
+                    <div>
+                        <div>
+                            <img src="" alt="">
+                        </div>
+                        <div>
+                            02/02/2022
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <img src="" alt="">
+                    <h2>A fidelidade às marcas está diminuindo: como manter os clientes engajados mesmo assim</h2>
+                    <p>Um estudo com 2.000 pessoas dos EUA e do Reino Unido pela Edit, especialista em dados de clientes, e Kin + Carta, uma co(...)</p>
+                    <div>
+                        <div>
+                            <img src="" alt="">
+                        </div>
+                        <div>
+                            02/02/2022
+                        </div>
+                    </div>
+                </article>
+            </section>
+        </section>
+
+        <section>
+            <div>
+                <div>
+                    <h3>Inscreva-se em nosso blog</h3>
+                    <p>Acesse, em primeira mão, nossos principais posts diretamente em seu email</p>
+                </div>
+                <div>
+                    <form action="" method="post">
+                        <input type="email" name="" id="" placeholder="E-mail">
+                        <button>Enviar</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <section>
+                <article>
+                    <img src="" alt="">
+                    <h3>A fidelidade às marcas está diminuindo: como manter os clientes engajados mesmo assim</h3>
+                    <div>
+                        <div>
+                            <img src="" alt="">
+                        </div>
+                        <div>
+                            02/02/2022
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <img src="" alt="">
+                    <h3>A fidelidade às marcas está diminuindo: como manter os clientes engajados mesmo assim</h3>
+                    <div>
+                        <div>
+                            <img src="" alt="">
+                        </div>
+                        <div>
+                            02/02/2022
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <img src="" alt="">
+                    <h3>A fidelidade às marcas está diminuindo: como manter os clientes engajados mesmo assim</h3>
+                    <div>
+                        <div>
+                            <img src="" alt="">
+                        </div>
+                        <div>
+                            02/02/2022
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <img src="" alt="">
+                    <h3>A fidelidade às marcas está diminuindo: como manter os clientes engajados mesmo assim</h3>
+                    <div>
+                        <div>
+                            <img src="" alt="">
+                        </div>
+                        <div>
+                            02/02/2022
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <img src="" alt="">
+                    <h3>A fidelidade às marcas está diminuindo: como manter os clientes engajados mesmo assim</h3>
+                    <div>
+                        <div>
+                            <img src="" alt="">
+                        </div>
+                        <div>
+                            02/02/2022
+                        </div>
+                    </div>
+                </article>
+
+                <div>
+                    <a href="#">Ver mais posts</a>
+                </div>
+            </section>
+
+            <section>
+                <h3>Escolha do Editor</h3>
+                <section>
+                    <article>
+                        <h4>Remarketing e retargeting: qual a diferença e quando usar cada estratégia</h4>
+                        <p>
+                            <div>
+                                <img src="" alt="">
+                            </div>
+                            <span>
+                                Redator Rock Content
+                            </span>
+                        </p>
+                    </article>
+                    <article>
+                        <h4>Remarketing e retargeting: qual a diferença e quando usar cada estratégia</h4>
+                        <p>
+                            <div>
+                                <img src="" alt="">
+                            </div>
+                            <span>
+                                Redator Rock Content
+                            </span>
+                        </p>
+                    </article>
+                </section>
+
+                <section>
+                    <article>
+                        <img src="" alt="">
+                    </article>
+                    <article>
+                        <img src="" alt="">
+                    </article>
+                    <article>
+                        <img src="" alt="">
+                    </article>
+                </section>
+            </section>
+        </section>
+
+    </main>
+
+    <footer>
+        <div>
+            <div>
+                <h3>
+                Quer receber mais conteúdos brilhantes como esse de graça?
+                </h3>
+                <h4>
+                Inscreva-se para receber nossos conteúdos por email e participe da comunidade da Rock Content!
+                </h4>
+            </div>
+
+            <div>
+                <form action="" method="post">
+                    <input type="text" name="" id="">
+                    <button>Enviar</button>
+                </form>
+            </div>
+        </div>
+        <div>
+            <div>
+                <p>
+                    <a href="#">
+                        <img src="" alt="">
+                    </a>
+                </p>
+                <p>
+                    <a href="#">Legal</a>
+                    <a href="#">Política de Privacidade</a>
+                </p>
+                <ul>
+                    <li>
+                        <a href="#"></a>
+                    </li>
+                    <li>
+                        <a href="#"></a>
+                    </li>
+                    <li>
+                        <a href="#"></a>
+                    </li>
+                    <li>
+                        <a href="#"></a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    <script src="./assets/js/jquery-3.6.0.min.js"></script>
+    <script src="./assets/js/owl.carousel.min.js"></script>
+    <script src="./assets/js/script.js"></script>
+    <script>
+        $(document).ready(function(){
+            $(".owl-carousel").owlCarousel({
+                loop:true,
+                margin:10,
+                nav:true,
+                responsive:{
+                    0:{
+                        items:1
+                    }   
+                }
+            });
+        });
+    </script>
+</body>
+</html>
