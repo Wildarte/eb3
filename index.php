d62bd26be93deeade5fad7657ffb09e2ce2bd3cd
wildarte <wildarte10@gmail.com>
2022-02-16 16:59:33 -0300

    fiz o footer

--- index.php
+++ index.php
@@ -338,9 +338,9 @@
 
     </main>
 
-    <footer>
-        <div>
-            <div>
+    <footer class="footer container-full">
+        <div class="footer_main container">
+            <div class="footer_left">
                 <h3>
                 Quer receber mais conteúdos brilhantes como esse de graça?
                 </h3>
@@ -349,36 +349,36 @@
                 </h4>
             </div>
 
-            <div>
+            <div class="footer_right">
                 <form action="" method="post">
-                    <input type="text" name="" id="">
-                    <button>Enviar</button>
+                    <input type="text" name="" id="" placeholder="E-mail...">
+                    <button class="footer_btn_submit">Enviar</button>
                 </form>
             </div>
         </div>
-        <div>
-            <div>
-                <p>
-                    <a href="#">
-                        <img src="" alt="">
+        <div class="footer_info container-full">
+            <div class="container footer_content_info">
+                <p class="footer_logo">
+                    <a class="footer_link_logo" href="#">
+                        <img src="./assets/img/logo.png" alt="">
                     </a>
                 </p>
-                <p>
+                <p class="footer_link_politicas">
                     <a href="#">Legal</a>
                     <a href="#">Política de Privacidade</a>
                 </p>
-                <ul>
+                <ul class="footer_social">
                     <li>
-                        <a href="#"></a>
+                        <a href="#"><i class="bi bi-facebook"></i></a>
                     </li>
                     <li>
-                        <a href="#"></a>
+                        <a href="#"><i class="bi bi-instagram"></i></a>
                     </li>
                     <li>
-                        <a href="#"></a>
+                        <a href="#"><i class="bi bi-twitter"></i></a>
                     </li>
                     <li>
-                        <a href="#"></a>
+                        <a href="#"><i class="bi bi-linkedin"></i></a>
                     </li>
 
                 </ul>
